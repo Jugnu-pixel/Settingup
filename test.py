d7f25fd3e99b187de53d3d7ad9e5e21ac23448f9
Jugnu-pixel <59956251+Jugnu-pixel@users.noreply.github.com>
2021-04-05 13:07:25 +0530

    test

--- test.py
+++ test.py
@@ -4,3 +4,46 @@ bulk comments can be put within tripple quotes
 '''
 print('Hello Kabeer')
 print('Welcome to a wonderful, healthy and happy life')
+A=10
+B="Jugnu"
+print(A,B)
+"""
+Tuples come within ()
+Lists come within []
+Dictionaries {'Age':41, 'Name':Jugnu}
+Set {A,1,2,B}
+"""
+X=10
+Y=15
+if(X<Y):
+    print('X is less than Y')
+elif('X>Y'):
+    print('X is greater than Y')
+else:
+    print('X is equal to Y')
+
+count=0
+while (count<5):
+    print('Jugnu')
+    count=count+1
+    break
+print('Done')
+exit
+
+
+
+Faccheck=0
+N=5
+count=1
+while (count<=N):
+    Faccheck=count*(count+1)
+print(Faccheck)
+
+
+
+
+
+
+
+
+
